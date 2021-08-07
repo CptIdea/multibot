f0555f54fe09de8593b6cb1da3c9f873a8bd974e
CptIdea <pliyznik@yandex.ru>
2021-08-07 17:20:09 +0300

    keyboard send fix

--- multibot.go
+++ multibot.go
@@ -1,7 +1,7 @@
 package multibot
 
 type Bot interface {
-	SendText(peer int,text string)error
-	SendKeyboard(peer int,text string, keyboard Keyboard)error
+	SendText(peer int, text string) error
+	SendKeyboard(peer int, text string, keyboard *Keyboard) error
 	GetMessagesChan() chan Message
 }
